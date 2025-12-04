78f6b843bb84871c52028cf179357c9d4b0639e1
Pierce Bartine <piercebartine@gmail.com>
2025-12-03 21:20:34 -0800

    2025 day 3 part 2

--- 2025/day03.py
+++ 2025/day03.py
@@ -1,53 +1,32 @@
 import unittest
-from typing import List, Optional, Tuple
+from typing import List
 
 
-def max_battery(bank: str) -> int:
-    top1: Optional[Tuple[int, str]] = None
+def max_joltage(bank: str, digits: int) -> int:
+    joltage = ""
 
-    for i, val in enumerate(bank):
-        if top1 is None:
-            top1 = (i, val)
-            continue
-        if val > top1[1]:
-            top1 = (i, val)
+    start = 0
+    while digits > 0:
+        end = len(bank) - digits + 1
 
-    if not top1:
-        raise Exception("unable to find top1")
+        # useless but safe comparison for the first iteration
+        max_i, max_digit = start, bank[start]
+        for i in range(start, end):
+            digit = bank[i]
+            if digit > max_digit:
+                max_i, max_digit = i, digit
 
-    # if top1 is all the way to the right, then top2 must be on the left
-    # otherwise just do max again starting after top1
-    reverse = False
-    if top1[0] == len(bank) - 1:
-        reverse = True
-        subbank = bank[: top1[0]]
-    else:
-        subbank = bank[top1[0] + 1 :]
+        joltage += max_digit
+        start = max_i + 1
+        digits -= 1  # drain digit count
 
-    top2: Optional[Tuple[int, str]] = None
-
-    for i, val in enumerate(subbank):
-        if top2 is None:
-            top2 = (i, val)
-            continue
-        if val > top2[1]:
-            top2 = (i, val)
-
-    if not top2:
-        raise Exception("unable to find top2")
-
-    if reverse:
-        return int(top2[1] + top1[1])
-    return int(top1[1] + top2[1])
+    return int(joltage)
 
 
-def part1(banks: List[str]) -> int:
+def solution(banks: List[str], digits: int) -> int:
     sum = 0
-
     for bank in banks:
-        battery = max_battery(bank)
-        sum += battery
-
+        sum += max_joltage(bank, digits)
     return sum
 
 
@@ -59,24 +38,23 @@ class Test(unittest.TestCase):
 818181911112111
 """
 
-    def test_max_battery(self):
-        self.assertEqual(max_battery("987654321111111"), 98)
-        self.assertEqual(max_battery("811111111111119"), 89)
-        self.assertEqual(
-            max_battery(
-                "1687783252471642886241864885417574245161768635576844528554852355566876467348472654541656343253424539"
-            ),
-            89,
-        )
-
     def test_part1_example(self):
         input = self.example_input.strip().splitlines()
-        self.assertEqual(part1(input), 357)
+        self.assertEqual(solution(input, 2), 357)
 
     def test_part1_real(self):
         with open("inputs/day03.txt", "r") as file:
             input = [line.strip() for line in file.readlines()]
-        self.assertEqual(part1(input), 17405)
+        self.assertEqual(solution(input, 2), 17405)
+
+    def test_part2_example(self):
+        input = self.example_input.strip().splitlines()
+        self.assertEqual(solution(input, 12), 3121910778619)
+
+    def test_part2_real(self):
+        with open("inputs/day03.txt", "r") as file:
+            input = [line.strip() for line in file.readlines()]
+        self.assertEqual(solution(input, 12), 171990312704598)
 
 
 if __name__ == "__main__":
