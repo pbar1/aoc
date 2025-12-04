87f00c25acf60c836801d533786a31185c917d4d
Pierce Bartine <piercebartine@gmail.com>
2025-12-03 23:43:58 -0800

    2025 day4 part 2 wip

--- 2025/day04.py
+++ 2025/day04.py
@@ -1,5 +1,4 @@
 import unittest
-from typing import Any, List
 
 
 def parse_grid(input: str) -> list[list[bool]]:
@@ -18,42 +17,7 @@ def occupied(grid: list[list[bool]], x: int, y: int) -> bool:
     return grid[y][x]
 
 
-def count_neighbors(grid: list[list[bool]]) -> list[list[int]]:
-    n_y: list[list[int]] = []
-    for y, row in enumerate(grid):
-        n_x: list[int] = []
-        for x, _ in enumerate(row):
-            # (x-1, y-1) | (x  , y-1) | (x+1, y-1)
-            # (x-1, y  ) |            | (x+1, y  )
-            # (x-1, y+1) | (x  , y+1) | (x+1, y+1)
-            checks = [
-                (x - 1, y - 1),
-                (x, y - 1),
-                (x + 1, y - 1),
-                (x - 1, y),
-                (x + 1, y),
-                (x - 1, y + 1),
-                (x, y + 1),
-                (x + 1, y + 1),
-            ]
-            neighbors = 0
-            for adj_x, adj_y in checks:
-                print(occupied(grid, adj_x, adj_y), adj_x, adj_y)
-                if occupied(grid, adj_x, adj_y):
-                    neighbors += 1
-            break
-            n_x.append(neighbors)
-        n_y.append(n_x)
-    return n_y
-
-
-def print_grid(a: List[List[Any]]):
-    width = max(len(str(int(x))) for row in a for x in row)
-    for row in a:
-        print(" ".join(f"{x:{width}}" for x in row))
-
-
-def solution(grid: list[list[bool]]) -> int:
+def accessible_rolls(grid: list[list[bool]]) -> int:
     total = 0
 
     print()
@@ -81,9 +45,47 @@ def solution(grid: list[list[bool]]) -> int:
             if adjacent_occupied < 4:
                 total += 1
             print(f"{'x' if adjacent_occupied < 4 else '@'}", end="")
-            # if y == 0 and x == 3:
-            #     print(adjacent_occupied)
         print()
+
+    return total
+
+
+def removable_rolls(grid: list[list[bool]], keep_going: bool) -> int:
+    total = 0
+
+    while True:
+        removed_queue = 0
+        print()
+        for y, row in enumerate(grid):
+            for x, cell in enumerate(row):
+                # empty cell never counts toward total
+                if not cell:
+                    print(".", end="")
+                    continue
+
+                checks = [
+                    (x - 1, y - 1),
+                    (x, y - 1),
+                    (x + 1, y - 1),
+                    (x - 1, y),
+                    (x + 1, y),
+                    (x - 1, y + 1),
+                    (x, y + 1),
+                    (x + 1, y + 1),
+                ]
+                adjacent_occupied = 0
+                for adj_x, adj_y in checks:
+                    if occupied(grid, adj_x, adj_y):
+                        adjacent_occupied += 1
+                if adjacent_occupied < 4:
+                    # remove the roll
+                    grid[y][x] = False
+                    removed_queue += 1
+                print(f"{'x' if adjacent_occupied < 4 else '@'}", end="")
+            print()
+        if removed_queue == 0 or not keep_going:
+            break
+        total += removed_queue
 
     return total
 
@@ -110,13 +112,23 @@ class Test(unittest.TestCase):
 
     def test_part1_example(self):
         grid = parse_grid(self.example)
-        self.assertEqual(solution(grid), 13)
+        self.assertEqual(accessible_rolls(grid), 13)
 
     def test_part1_real(self):
         with open("inputs/day04.txt", "r") as file:
             input = file.read().strip()
         grid = parse_grid(input)
-        self.assertEqual(solution(grid), 1363)
+        self.assertEqual(accessible_rolls(grid), 1363)
+
+    def test_part2_example(self):
+        grid = parse_grid(self.example)
+        self.assertEqual(removable_rolls(grid, keep_going=True), 43)
+
+    # def test_part2_real(self):
+    #     with open("inputs/day04.txt", "r") as file:
+    #         input = file.read().strip()
+    #     grid = parse_grid(input)
+    #     self.assertEqual(removable_rolls(grid), -1)
 
 
 if __name__ == "__main__":
