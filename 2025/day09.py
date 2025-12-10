e60efc85cee72f4dfade0de25f8e8e35a5ce0bb8
Pierce Bartine <piercebartine@gmail.com>
2025-12-09 23:15:21 -0800

    2025 day 9 input

---
+++ 2025/day09.py
@@ -0,0 +1,52 @@
+import unittest
+
+
+def parse_input(input: str) -> list[tuple[int, int]]:
+    points: list[tuple[int, int]] = []
+    for line in input.strip().splitlines():
+        [x, y] = line.split(",")
+        points.append((int(x), int(y)))
+    return points
+
+
+def solve_part1(input: list[tuple[int, int]]) -> int:
+    return -1
+
+
+def solve_part2(input: list[tuple[int, int]]) -> int:
+    return -1
+
+
+class Test(unittest.TestCase):
+    example = """
+7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3
+""".strip()
+
+    def test_part1_example(self):
+        input = parse_input(self.example)
+        self.assertEqual(solve_part1(input), 50)
+
+    def test_part1_real(self):
+        with open("inputs/day09.txt", "r") as file:
+            input = parse_input(file.read().strip())
+        self.assertEqual(solve_part1(input), -1)
+
+    def test_part2_example(self):
+        input = parse_input(self.example)
+        self.assertEqual(solve_part2(input), -1)
+
+    def test_part2_real(self):
+        with open("inputs/day09.txt", "r") as file:
+            input = parse_input(file.read().strip())
+        self.assertEqual(solve_part2(input), -1)
+
+
+if __name__ == "__main__":
+    unittest.main()
