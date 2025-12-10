8afb88c23ce7db5552665b264b087e01a467131b
Pierce Bartine <piercebartine@gmail.com>
2025-12-09 22:12:51 -0800

    2025 day 8 part 1 solved

--- 2025/day08.py
+++ 2025/day08.py
@@ -22,42 +22,68 @@ def distance(a: Point, b: Point) -> float:
     return ((a.x - b.x) ** 2 + (a.y - b.y) ** 2 + (a.z - b.z) ** 2) ** 0.5
 
 
-def solve(points: list[Point]) -> int:
-    friends: dict[Point, Tuple[Point, float]] = {}
-
+def solve(points: list[Point], top: int) -> int:
+    segments: list[Tuple[float, Point, Point]] = []
     for a in points:
-        shortest: Optional[float] = None
         for b in points:
             if a == b:
                 continue
             d = distance(a, b)
-            if not shortest or d < shortest:
-                shortest = d
-                friends[a] = (b, d)
+            segments.append((d, a, b))
+    segments.sort()
+    # otherwise all the point info would be duplicated because we took the
+    # cartesian product
+    segments = [segment for i, segment in enumerate(segments) if i % 2 == 0]
 
-    friend_list: list[Tuple[float, Point, Point]] = []
-    for a, (b, d) in friends.items():
-        friend_list.append((d, a, b))
-    friend_list.sort()
+    # we only want the top N segments
+    segments = segments[:top]
+    # for segment in segments:
+    #     print(segment)
 
     sets: list[set[Point]] = []
-    for _, a, b in friend_list:
-        added = False
+
+    # create maximal sets
+
+    for _, a, b in segments:
+        append_new_set = True
         for s in sets:
             if a in s or b in s:
                 s.add(a)
                 s.add(b)
-                added = True
+                append_new_set = False
                 break
-        if not added:
+        if append_new_set:
             sets.append({a, b})
 
-    sets.sort(key=lambda x: len(x), reverse=True)
+    # merge the sets
 
-    for s in sets:
-        print(len(s), s)
+    keep_going = True
+    while keep_going:
+        # pessimistically assume that this is the last iteration
+        keep_going = False
 
-    return -1
+        merged_sets: list[set[Point]] = []
+        for s in sets:
+            for merged in merged_sets:
+                # sets overlap, so merge s into merged
+                if not s.isdisjoint(merged):
+                    merged.update(s)
+                    keep_going = True
+                    break
+            else:
+                merged_sets.append(s)
+
+        sets = merged_sets
+
+    # count total
+
+    sets.sort(key=lambda s: len(s), reverse=True)
+    total = 1
+    for s in sets[:3]:
+        # print(len(s), s)
+        total *= len(s)
+
+    return total
 
 
 class Test(unittest.TestCase):
@@ -89,14 +115,19 @@ class Test(unittest.TestCase):
         b = Point(425, 690, 689)
         self.assertAlmostEqual(distance(a, b), 316.90219311327)
 
+    def test_distance_2(self):
+        a = Point(431, 825, 988)
+        b = Point(425, 690, 689)
+        self.assertAlmostEqual(distance(a, b), 328.11888089532425)
+
     def test_part1_example(self):
         points = parse_input(self.example)
-        self.assertEqual(solve(points), 40)
+        self.assertEqual(solve(points, top=10), 40)
 
-    # def test_part1_real(self):
-    #     with open("inputs/day08.txt", "r") as file:
-    #         points = parse_input(file.read().strip())
-    #     self.assertEqual(solve(points), -1)
+    def test_part1_real(self):
+        with open("inputs/day08.txt", "r") as file:
+            points = parse_input(file.read().strip())
+        self.assertEqual(solve(points, top=1000), 63920)
 
 
 if __name__ == "__main__":
